befed46e7c9c2a78b3566d0b382c28a0cf6b6e32
karthikbsel <69590699+karthikbsel@users.noreply.github.com>
2021-04-30 12:13:29 +0530

    commit

---
+++ Abstractdemo.java
@@ -0,0 +1,15 @@
+
+public class Abstractdemo extends class1 {
+
+	public static void main(String[] args) {
+		
+		class1 obj= new Abstractdemo();
+		obj.adhar();
+		obj.haveBreakfast();
+			}
+	
+	void haveBreakfast() {
+		System.out.println("have idli");
+	}
+
+}
